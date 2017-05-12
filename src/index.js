f231494e7982ea67c541acca8fddd9326fc6275f
Runnan Zhong <r.tina.zhong@gmail.com>
2017-05-12 10:40:46 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 $(document).ready(function(){
   // your code here!
+
+  $('#photo-form').on('submit', function(event){
+    event.preventDefault();
+
+  var imageURL = $("#image_input").val()
+  var captionInput = $("#caption_input").val()
+  // $('#image_input').on('submit', `${imageURL}` function())
+
+  // $('body').on('click', '.delete-button', function(){
+  //   this.parentElement.remove()
+  //   console.log('here');
+  //
+  //   })
+
+    renderImage = $('#photo-list').append(`<div><image scr= "${imageURL}" id = 'photo-id'></div>`)
+    // make "#photo-id dynamic, make a model?"
+    $('#photo-id').append(`<p>${captionInput}</p>`)
+
+
+
+    // $('#photo-form').on('click', `#submit_button`, function(){
+    //   $('#photo-list').append(`<ul>${imageURL}</ul>`)
+    //
+    // })
+
+    $("#image_input").val('')
+    $("#caption_input").val('')
+
+  })
+
+
+  // $('body').on('click', `#submit_button`, function(){
+  //   $('#photo-list').append(`${captionRender}`)
+  //
+  // })
+
 })
